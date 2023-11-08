378d5f6d0ddf6c6ead7f95c77858446b9cc14d7f
Victor is Geek <92975790+victorgeel@users.noreply.github.com>
2023-11-09 04:08:21 +0630

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-python -m pip install -U --no-cache-dir --disable-pip-version-check pip setuptools wheel &&
-python -m pip install -U --no-cache-dir --disable-pip-version-check -r requirements.txt &&
-python -m proxy_scraper_checker
+python3 -m pip install -U --no-cache-dir --disable-pip-version-check pip setuptools wheel &&
+python3 -m pip install -U --no-cache-dir --disable-pip-version-check -r requirements.txt &&
+python3 -m proxy-here-Noobs
